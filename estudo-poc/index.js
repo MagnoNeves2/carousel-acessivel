b3dff78f31fbdd835fe7878a0ff4e743d737b19a
Magno Neves <magnoneves2@gmail.com>
2021-11-22 20:15:38 -0300

    organização para github pages

--- estudo-poc/index.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('ul').addEventListener('wheel', (event) => {
-    if(event.deltaY < 0) {
-        event.target.scrollBy(300, 0);
-    } else {
-        event.target.scrollBy(-300, 0);
-    }
-});
