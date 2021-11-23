996ecfc3bc911819b591269db839eec34ca50f89
Magno Neves <magnoneves2@gmail.com>
2021-11-23 13:18:09 -0300

    alterando o atributo de listagem desordenado para ordenado

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-document.querySelector("ul").addEventListener("wheel", (event) => {
-	if(event.deltaY < 0) {
-		event.target.scrollBy(200, 0);
-	} else {
-		event.target.scrollBy(-200, 0);
-	}
+document.querySelector("ol").addEventListener("wheel", (event) => {
+  if (event.deltaY < 0) {
+    event.target.scrollBy(200, 0);
+  } else {
+    event.target.scrollBy(-200, 0);
+  }
 });
 
-linkar = (event) => {
-const title = event.target.innerText;
-document.querySelector(".doubt").textContent = `Dúvidas em ${title}`;
-console.log(title);
+link = (event) => {
+  const title = event.target.innerText;
+  document.querySelector(".doubt").textContent = `Dúvidas em ${title}`;
 
-window.location.href = "#doubt-title";
+  window.location.href = "#doubt-title";
+  cardSelected(title);
+};
+
+cardSelected = (title) => {
+  const productsList = document.querySelector(".products-list").children;
+
+  for (let i = 0; i < productsList.length; i++) {
+    const card = productsList[i];
+    const cardTitle = card.textContent.trim();
+
+    if (cardTitle === title) {
+      console.log(`Posição do card: ${i}`);
+    }
+  }
 };
