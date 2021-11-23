04ca91810fe99f1aa4a9572eaabc80f161e4c182
Magno Neves <magnoneves2@gmail.com>
2021-11-23 17:15:17 -0300

    habilitando o aria-label dos itens do carousel

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const list = document.querySelector("ul");
+
 document.querySelector("ul").addEventListener("wheel", (event) => {
   if (event.deltaY < 0) {
     event.target.scrollBy(200, 0);
@@ -6,23 +8,38 @@ document.querySelector("ul").addEventListener("wheel", (event) => {
   }
 });
 
+document.querySelector("ul").addEventListener('mouseenter', () => {
+  const list = document.querySelector("ul").children;
+
+  for (let i = 0; i < list.length; i++) {
+		console.log();
+		list[i].ariaLabel = `card ${i + 1} de ${list.length} cards`;
+  }
+})
+
+// const title = document.querySelector("button").textContent.trim();
+  // console.log(title);
+
+  // for (let i = 0; i < list.length; i++) {
+  //   const card = list[i];
+  //   const cardTitle = card.textContent.trim();
+  //   console.log(cardTitle === title);
+
+  //   if (cardTitle === title) {
+  //     console.log(`Posição do card: ${i + 1}`);
+  //     // card.ariaLabel = `card ${i + 1} de ${productsList.length}`
+  //   }
+  // }
+
 link = (event) => {
   const title = event.target.innerText;
   document.querySelector(".doubt").textContent = `Dúvidas em ${title}`;
 
   window.location.href = "#doubt-title";
-  cardSelected(title);
+  cardSelected(event);
 };
 
-cardSelected = (title) => {
-  const productsList = document.querySelector(".products-list").children;
-
-  for (let i = 0; i < productsList.length; i++) {
-    const card = productsList[i];
-    const cardTitle = card.textContent.trim();
-
-    if (cardTitle === title) {
-      console.log(`Posição do card: ${i + 1}`);
-    }
-  }
+cardSelected = (event) => {
+  // TODO: Mudar a cor da div de seleção.
+	event.target.ariaSelected = "true";
 };
