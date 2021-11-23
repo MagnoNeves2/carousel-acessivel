993c31b07be85fb382e60842184054e5cc28693a
Magno Neves <magnoneves2@gmail.com>
2021-11-23 20:50:18 -0300

    criação e implementação da lógica de scroll das bolinhas/setinhas

--- index.js
+++ index.js
@@ -8,28 +8,24 @@ document.querySelector("ul").addEventListener("wheel", (event) => {
   }
 });
 
-document.querySelector("ul").addEventListener('mouseenter', () => {
+document.querySelector("ul").addEventListener("mouseenter", () => {
   const list = document.querySelector("ul").children;
 
   for (let i = 0; i < list.length; i++) {
-		console.log();
-		list[i].ariaLabel = `card ${i + 1} de ${list.length} cards`;
+    console.log();
+    list[i].ariaLabel = `card ${i + 1} de ${list.length} cards`;
   }
-})
+});
 
-// const title = document.querySelector("button").textContent.trim();
-  // console.log(title);
+// console.log(document.querySelector(".products-section").clientWidth);
+// console.log(document.querySelector(".products-list"));
+// console.log(document.querySelector(".card-box").clientWidth * 5);
 
-  // for (let i = 0; i < list.length; i++) {
-  //   const card = list[i];
-  //   const cardTitle = card.textContent.trim();
-  //   console.log(cardTitle === title);
-
-  //   if (cardTitle === title) {
-  //     console.log(`Posição do card: ${i + 1}`);
-  //     // card.ariaLabel = `card ${i + 1} de ${productsList.length}`
-  //   }
-  // }
+const bulletAmount =
+  Math.floor(
+    (document.querySelector(".card-box").clientWidth * 5) /
+      document.querySelector(".products-section").clientWidth
+  ) + 1;
 
 link = (event) => {
   const title = event.target.innerText;
@@ -41,5 +37,41 @@ link = (event) => {
 
 cardSelected = (event) => {
   // TODO: Mudar a cor da div de seleção.
-	event.target.ariaSelected = "true";
+  event.target.ariaSelected = "true";
+};
+
+let selectedBullet = 0;
+let scrollTotal = 0;
+
+scrollCarousel = (bullet) => {
+  if (bullet != selectedBullet) {
+    let scrollTarget = 0;
+
+    // if (bullet > selectedBullet) {
+      const diffBullet = bullet - selectedBullet;
+      scrollTarget =
+        document.querySelector(".products-section").clientWidth * diffBullet;
+    // }
+
+    // if (bullet < selectedBullet) {
+    //   const diffBullet = bullet - selectedBullet;
+    //   scrollTarget =
+    //     document.querySelector(".products-section").clientWidth * diffBullet;
+    // }
+
+    document.querySelector(".products-list").scrollBy(scrollTarget, 0);
+
+    selectedBullet = bullet;
+    scrollTotal = scrollTarget;
+  }
+};
+
+createBullets = () => {
+  for (let i = 0; i < bulletAmount; i++) {
+    const button = document.createElement("button");
+    button.innerHTML = "bolinha " + (i + 1);
+    button.onclick = () => scrollCarousel(i);
+
+    document.querySelector("#products-section").append(button);
+  }
 };
